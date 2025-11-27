f82b6e558a25d1329392614c3937869ba3388fd6
AlshimaaAhmed <alshimaa185@gmail.com>
2025-11-27 20:45:48 +0200

    Catalog and Cart logic

---
+++ docs/Cartjson.fs
@@ -0,0 +1,50 @@
+module CartJson
+
+open System
+open System.IO
+open System.Text.Json
+open Cart
+
+type CartItemDto = {
+    ProductId: int
+    ProductName: string
+    UnitPrice: decimal
+    Quantity: int
+}
+
+type CartDto = {
+    Items: CartItemDto[]
+    Total: decimal
+}
+
+let private toDto (item: Cart.CartItem) =
+    { ProductId = item.ProductId; ProductName = item.ProductName; UnitPrice = item.Unitprice; Quantity = item.Quantity }
+
+let cartToDto (cart: Cart.Cart) : CartDto =
+    let items = cart |> toList |> List.map toDto |> List.toArray
+    let total = getTotalPrice cart
+    { Items = items; Total = total }
+
+let cartToJson (cart: Cart) : string =
+    cartToDto cart |> JsonSerializer.Serialize
+
+let saveCartToFile (path:string) (cart: Cart) =
+    let json = cartToJson cart
+    File.WriteAllText(path, json)
+
+// load cart from json
+let loadCartFromJson (json:string) : Cart.Cart =
+    let dtoOpt = JsonSerializer.Deserialize<CartDto>(json) |> Option.ofObj
+    match dtoOpt with
+    | None -> Map.empty
+    | Some dto ->
+        dto.Items
+        |> Array.fold (fun acc i ->
+            acc |> Map.add i.ProductId {ProductId = i.ProductId; ProductName = i.ProductName; Unitprice = i.UnitPrice; Quantity = i.Quantity }
+        ) Map.empty
+
+let loadCartFromFile (path:string) : Cart =
+    if File.Exists(path) then
+        let json = File.ReadAllText(path)
+        loadCartFromJson json
+    else Map.empty
---
+++ docs/Program.fs
@@ -0,0 +1,52 @@
+﻿open Catalog
+open Cart
+open CartJson
+
+[<EntryPoint>]
+let main argv = 
+    let catalog = loadCatalog()
+
+    printfn "       === All Products (%d) ===" (catalog.Count)
+    getAllProducts catalog 
+          |> List.iter (fun p -> printfn "%d: %s (%s) - Price: %M - Stock: %d" p.Id p.Name p.Category p.Price p.Stock)
+
+
+    printfn "\n=== CART TEST ==="
+
+   // let the cart mutable just for testing
+    let mutable cart = Cart.empty
+
+    cart <- Cart.addItem catalog 1 1 cart    // Laptop ×1
+    cart <- Cart.addItem catalog 2 2 cart    // Mouse ×2
+    cart <- Cart.addItem catalog 8 3 cart    // Sugar ×3
+
+    printfn "\n--- Items in cart ---"
+    Cart.toList cart |> List.iter (fun i ->
+        printfn "ProductId:%d  %s  Qty:%d  Unit:%M" 
+            i.ProductId i.ProductName i.Quantity i.Unitprice
+    )
+
+    printfn "\nTotal = %M" (Cart.getTotalPrice cart)
+
+    printfn "\n--- Saving cart.json ---"
+    saveCartToFile "cart.json" cart
+    printfn "JSON Saved successfully!"
+
+
+
+    printfn "\n=== Find By Id ==="
+    match findById 100 catalog with
+    | Some p -> printfn "Found: %d - %s" p.Id p.Name
+    | None -> printfn "Not Found"
+
+
+    printfn "\n=== Filter By Category ==="
+    filterByCategory "Grocery" catalog
+    |> List.iter (fun p -> printfn "%d: %s - %M" p.Id p.Name p.Price)
+
+
+    printfn "\n=== Search by Name ==="
+    searchByName "book" catalog
+    |> List.iter (fun p -> printfn "%d: %s - %M" p.Id p.Name p.Price)
+
+    0
